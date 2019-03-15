006a1600d62df1b80540164881daabc96ffd97bd
SmallhillCZ <github@smallhill.cz>
2019-03-15 17:02:19 +0100

    Add return type

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { LineSplit, RecordMerger, RecordParser } from "./transformers";
 import * as multipipe from "multipipe";
+import { Duplex } from "stream";
 
-export function kxxreader() {
+export function kxxreader(): Duplex {
 
-  multipipe(
+  return multipipe(
     new LineSplit(), // split stream into lines
     new RecordMerger(), // merge lines that form a record
     new RecordParser(), // parse lines into JSON record with data
